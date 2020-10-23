92aac6a83d4246b7e3b21da1ee8f190401b3c448
Suja1996 <72971591+Suja1996@users.noreply.github.com>
2020-10-23 12:31:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -75,7 +75,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if(keyDown("space") && trex.y >= height-120 || touches.length>0) {
+    if(keyDown("space") && trex.y >= height-120 || touches.length>0 && trex.y >= height-120 ) {
       trex.velocityY = -12;
       touches=[];
     }
